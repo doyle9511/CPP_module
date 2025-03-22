059728a6222a615ab34f40633832546d4a771f3c
Donghwi Kim <donghwi2@c2r4s2.42gyeongsan.kr>
2025-03-22 19:38:37 +0900

    asdf

--- cpp_03/ex03/main.cpp
+++ cpp_03/ex03/main.cpp
@@ -6,33 +6,13 @@
 /*   By: donghwi2 <donghwi2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:17:22 by donghwi2          #+#    #+#             */
-/*   Updated: 2025/03/22 19:23:15 by donghwi2         ###   ########.fr       */
+/*   Updated: 2025/03/22 19:35:42 by donghwi2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 int main(void) {
-	std::cout << "\n===== ClapTrap 테스트 =====" << std::endl;
-	ClapTrap ct("CT-1");
-	ct.attack("적");
-	std::cout << "ClapTrap 체력: " << ct.getHitPoint() << ", 에너지: " << ct.getEnergyPoint() << ", 공격력: " << ct.getAttackDamage() << std::endl;
-	std::cout << std::endl;
-		
-	std::cout << "===== ScavTrap 테스트 =====" << std::endl;
-	ScavTrap st("ST-1");
-	st.attack("적");
-	st.guardGate();
-	std::cout << "ScavTrap 체력: " << st.getHitPoint() << ", 에너지: " << st.getEnergyPoint() << ", 공격력: " << st.getAttackDamage() << std::endl;
-	std::cout << std::endl;
-		
-	std::cout << "===== FragTrap 테스트 =====" << std::endl;
-	FragTrap ft("FT-1");
-	ft.attack("적");
-	ft.highFivesGuys();
-	std::cout << "FragTrap 체력: " << ft.getHitPoint() << ", 에너지: " << ft.getEnergyPoint() << ", 공격력: " << ft.getAttackDamage() << std::endl;
-	std::cout << std::endl;
-		
 	std::cout << "===== DiamondTrap 테스트 =====" << std::endl;
 	DiamondTrap dt("DT-1");
 	dt.attack("적");		  // ScavTrap의 attack 사용
